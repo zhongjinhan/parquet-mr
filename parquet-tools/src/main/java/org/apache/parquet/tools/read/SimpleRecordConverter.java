f1619750e2adeabb9abe66eb4450a937e95572d3
dsfcode <fowler.dn@gmail.com>
2017-05-12 14:40:29 -0700

    Backport PARQUET-969: Update parquet-tools to convert Decimal datatype to BigD…
    Update parquet-tools so that decimal datatypes in parquet files are converted to their actual number representation when cat'ing to stdout. Currently they are output in binary format.
    
    Author: dsfcode <fowler.dn@gmail.com>
    
    Closes #412 from dsfcode/master and squashes the following commits:
    
    7f05509 [dsfcode] PARQUET-969: Update parquet-tools to convert Decimal datatype to BigDecimal
    
    (cherry picked from commit a703ee75c40e0207f6831c4d48e1c7e62f160305)
    
    Change-Id: Iaf47a34538004580c91799cc478c98b20309b2d5

--- parquet-tools/src/main/java/org/apache/parquet/tools/read/SimpleRecordConverter.java
+++ parquet-tools/src/main/java/org/apache/parquet/tools/read/SimpleRecordConverter.java
@@ -63,6 +63,12 @@ public class SimpleRecordConverter extends GroupConverter {
           public Optional<Converter> visit(LogicalTypeAnnotation.StringLogicalTypeAnnotation stringLogicalType) {
             return of(new StringConverter(field.getName()));
           }
+
+          @Override
+          public Optional<Converter> visit(LogicalTypeAnnotation.DecimalLogicalTypeAnnotation decimalLogicalType) {
+            int scale = decimalLogicalType.getScale();
+            return of(new DecimalConverter(field.getName(), scale));
+          }
         }).orElse(new SimplePrimitiveConverter(field.getName()));
       }
     }
@@ -151,6 +157,20 @@ public class SimpleRecordConverter extends GroupConverter {
     @Override
     public void addBinary(Binary value) {
       record.add(name, value.toStringUsingUTF8());
+    }
+  }
+
+  private class DecimalConverter extends SimplePrimitiveConverter {
+    private final int scale;
+
+    public DecimalConverter(String name, int scale) {
+      super(name);
+      this.scale = scale;
+    }
+
+    @Override
+    public void addBinary(Binary value) {
+      record.add(name, new BigDecimal(new BigInteger(value.getBytes()), scale));
     }
   }
 }
