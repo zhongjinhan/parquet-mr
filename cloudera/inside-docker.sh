187ee389e4c05652357d317364002ee423d2160b
cloudera <cauldron-dev@cloudera.com>
2019-01-23 03:24:02 -0800

    CLOUDERA-BUILD. Preparing for 6.1.1 release.
    Commit performed on https://master-01.jenkins.cloudera.com/
    
    Signed-off-by: cloudera <cauldron-dev@cloudera.com>

--- cloudera/inside-docker.sh
+++ cloudera/inside-docker.sh
@@ -16,7 +16,7 @@ if [[ ! -d "$PROTOC_HOME" ]]; then
     exit 1
 fi
 
-CURRENT_BRANCH=cdh6.1.x
+CURRENT_BRANCH=cdh6.1.1
 
 # we need to re-run setup inside the docker container to get mvn-gbn script.
 SETUP_FILE="$(mktemp)"
